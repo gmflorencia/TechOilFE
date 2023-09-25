370278437972dc16a8641d92b6ab16ce8f506a8e
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-25 20:51:15 -0300

    Agregar archivos de proyecto.

---
+++ TechOilFE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechOilFE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
