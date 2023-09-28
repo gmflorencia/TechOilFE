804b9dfa0ffc2d65872ed753cde9248b97138474
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-27 21:57:35 -0300

    Creación de login

--- Data/DTOs/LoginDto.cs
+++ Data/DTOs/LoginDto.cs
@@ -9,7 +9,7 @@ namespace Data.DTOs
     public class LoginDto
     {
 
-        public string Mail { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
     }
 }
