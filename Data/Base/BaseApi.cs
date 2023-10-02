9ee317b96dcafceacfad8e217f835d1b7cb2090e
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-02 02:58:48 -0300

    Creacion de DTOs, Base y sln

--- Data/Base/BaseApi.cs
+++ Data/Base/BaseApi.cs
@@ -18,8 +18,6 @@ namespace Data.Base
         {
             _httpClient = httpClient;
         }
-
-
         public async Task<IActionResult> PostToApi(string controllerName, object model, string token = "")
         {
             try
@@ -32,6 +30,60 @@ namespace Data.Base
                 }
 
                 var response = await client.PostAsJsonAsync(controllerName, model);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                }
+
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+        public async Task<IActionResult> PutToApi(string controllerName, object model, string token = "")
+        {
+            try
+            {
+                var client = _httpClient.CreateClient("useApi");
+
+                if (token != "")
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+                }
+
+                var response = await client.PutAsJsonAsync(controllerName, model);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                }
+
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+        public async Task<IActionResult> DeleteToApi(string controllerName, string token = "")
+        {
+            try
+            {
+                var client = _httpClient.CreateClient("useApi");
+
+                if (token != "")
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+                }
+
+                var response = await client.DeleteAsync(controllerName);
 
                 if (response.IsSuccessStatusCode)
                 {
---
+++ Data/DTOs/UsuarioDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DTOs
+{
+    public class UsuarioDto
+    {
+        public int CodUsuario { get; set; }
+        public string Nombre { get; set; }
+        public int Dni { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public string? Rol { get; set; }
+        public int CodRol { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
